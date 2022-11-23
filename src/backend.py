ede5f87c93fe6942506faca4896a7fc03a5c17da
Slluxx <Slluxx@users.noreply.github.com>
2022-11-23 17:43:50 +0100

    Improvements
    - dumped pokemon are not written to disk and read again
    - changed dump filename to box_pos_pokm name
    - added better log function with word wrap
    - fixed issue in the injection method where even if an exception occurs, the pokemon would still get injected.
    - general code cleanup

--- src/backend.py
+++ src/backend.py
@@ -1,9 +1,5 @@
-import asyncio
-import threading
-import traceback
-import re
-import random
-import string
+import asyncio, threading
+import re, random, string, traceback, datetime
 import dearpygui.dearpygui as dpg
 from px8parse import *
 from socketSwitch import *
@@ -15,6 +11,11 @@ pk_config = {
     "box_array": 0x158,
     "pk_size": 344
 }
+
+def log2window(message, window="logging_window"):
+    now = datetime.datetime.now().strftime("%H:%M:%S")
+    dpg.add_text(f"[{now}] {message}\n", parent=window, wrap=480)
+    dpg.set_y_scroll(window, 999999)
 
 def dpg_callback(sender: bool = False, app_data: bool = False, user_data: bool = False):
     def decorator(function):
@@ -46,42 +47,50 @@ async def backend_connect(sender, app_data, user_data):
     switch_ip = dpg.get_value("switch_ip")
     switch_port = dpg.get_value("switch_port")
     if not re.match(r"^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$", switch_ip):
-        return dpg.add_text(f"[!] {switch_ip} is not a valid IP.", parent="logging_window")
+        log2window(f"{switch_ip} is not a valid IP.")
+        return False
 
-    dpg.add_text(f"Connecting to {switch_ip}", parent="logging_window")
-    try:
-        socketSwitch.setConnection(switch_ip, switch_port)
-        socketSwitch.connect()
-    except socket.error:
-        dpg.add_text(f"[!] Unable to connect to {switch_ip}:{switch_port}.", parent="logging_window")
-    print(sender, app_data, user_data)
+    log2window(f"Connecting to {switch_ip}:{switch_port}...")
+
+    socketSwitch.setConnection(switch_ip, switch_port)
+    ret = socketSwitch.connect()
+    log2window(ret)
 
 
 @dpg_callback(sender=True, app_data=True, user_data=True)
 async def backend_dump(sender, app_data, user_data):
-    name = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10))
+    boxNumber = dpg.get_value('boxnumber')
+    positionInBox = dpg.get_value('posInBox')
+
+    log2window(f"Dumping box {boxNumber} position {positionInBox}...")
+    
     pokemon = socketSwitch.recv(f"pointerPeek 344 {pk_config['offset']}")
-    with open(f"{name}.ek9", "wb+") as writer:
+    pkmDataStr = str(PX8(buf = bytes.fromhex(pokemon.decode("utf-8"))))
+    pkmName = pkmDataStr.split("\n")[0].split(" ")[0]
+
+    if not pkmName or len(pkmName)<2:
+        log2window(f"Unable to dump Pokemon.")
+        return False
+
+    filename = f"{boxNumber}_{positionInBox}_{pkmName.lower()}.pk8"
+    with open(f"{filename}.ek9", "wb+") as writer:
         try:
             writer.write(bytes.fromhex(pokemon.decode("utf-8")))
-            dpg.add_text(f"Dumped pokemon {dpg.get_value('posInBox')} from box {dpg.get_value('boxnumber')} to {name}.ek9.", parent="logging_window")
-        except Exception as e:
-            return dpg.add_text(f"[!] Dump failed.", parent="logging_window")
-    with open(f"{name}.ek9", "rb") as reader:
-        try:
-            dpg.add_text(str(PX8(buf = reader.read())), parent="logging_window")
+            log2window(f"Dumped '{pkmName}' to {filename}")
         except:
-            pass
-    print(sender, app_data, user_data)
+            log2window(f"Unable to dump pokemon.")
 
 
 @dpg_callback(sender=True, app_data=True, user_data=True)
 async def backend_inject(sender, app_data, user_data):
+    box = dpg.get_value('boxnumber')
+    pos = dpg.get_value('posInBox')
+
     try:
         with open(app_data["file_name"], "rb") as f:
             pk = f.read().hex()
-    except Exception as e:
-        return dpg.add_text(f"[!] Unable to open file {e}", parent="logging_window")
-    pokemon = socketSwitch.send(f"pointerPoke 0x{pk} {pk_config['offset']}")
-    dpg.add_text(f"Injecting pokemon {dpg.get_value('posInBox')} from box {dpg.get_value('boxnumber')}", parent="logging_window")
-    print(sender, app_data, user_data)
+            log2window(f"Injecting {app_data['file_name']} into box {box} position {pos}...")
+            pokemon = socketSwitch.send(f"pointerPoke 0x{pk} {pk_config['offset']}")
+    except Exception:
+        log2window(f"Unable to open file")
+        return False
